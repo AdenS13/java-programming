1c0e785102b02be0306242a1a15ff9813f7d16aa
Aden Smith <adensmithy13@gmail.com>
2026-08-13 19:36:45 -0700

    Day 1: Java Fundamentals

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main   {
+    public static void main(String[] args)  {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.println("How old are you?");
+            int age = scanner.nextInt();
+                if(age >= 18) {
+                    System.out.println("You are an adult!");
+            }   else if(age == 0){
+                    break;
+            }   else if(age < 0) {
+                    System.out.println("That isn't a valid age.");
+            }   else if(age < 13) {
+                    System.out.println("You are a child!");
+            }   else {
+                    System.out.println("You are a teenager!");
+        }
+        }
+    }
+}
