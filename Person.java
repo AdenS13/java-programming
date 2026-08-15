895246a8f65a56bf76d03a2578b88debce3665f8
Aden Smith <adensmithy13@gmail.com>
2026-08-14 18:17:50 -0700

    W1D2 Java OOP Practice

--- Person.java
+++ Person.java
@@ -2,5 +2,22 @@
 public class Person {
     String name;
     int age;
+    
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
+    public void introduce() {
+        System.out.println("Hi, my name is " + name + " and I am " + age + " years old. ");
+    }
+
+    public void celebrateBirthday() {
+        age = age + 1;
+        System.out.println("Happy birthday " + name + "! You are now " + age + ".");
+    }
+
+    public int getBirthYear() {
+        return 2026 - age;
+    }
 }
--- w1d2.java
+++ w1d2.java
@@ -3,23 +3,19 @@ import java.util.Scanner;
 
 public class w1d2 {
 
-    public static int calculateAgeInFiveYears(int age) {
-        return age + 5;
-    }
-    
-    public static void greetUser(String name, int age) {
-        System.out.println("Hello " + name);
-        System.out.println("Welcome to my Java program!");
-        System.out.println("You will be " + calculateAgeInFiveYears(age) + " in five years!");
-    }
-
     public static void main(String[] args) {
-        Person person = new Person();
-        person.name = "Aden";
-        person.age = 23;
-        System.out.println("What is your name?");
-        System.out.println("How old are you??");
-        greetUser(person.name, person.age);
+        Person person1 = new Person("Aden", 23);
+        Person person2 = new Person("Sarah", 25);
+        int birthYear1 = person1.getBirthYear();
+        int birthYear2 = person2.getBirthYear();
 
+        person1.introduce();
+        person2.introduce();
+        
+        person1.celebrateBirthday();
+        person2.celebrateBirthday();
+
+        System.out.println(person1.name + " was born in " + birthYear1);
+        System.out.println(person2.name + " was born in " + birthYear2); 
     }
 }
