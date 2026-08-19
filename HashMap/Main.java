b8d1e2bf7df04f9212ff2420a00da6be13bffdc6
Aden Smith <adensmithy13@gmail.com>
2026-08-18 18:32:44 -0700

    Map.Entry Practice

--- HashMap/Main.java
+++ HashMap/Main.java
@@ -1,3 +1,7 @@
+import java.util.Map;
+
+import javax.lang.model.element.QualifiedNameable;
+
 import java.util.HashMap;
 
 public class Main {
@@ -6,6 +10,9 @@ public class Main {
 
         String searchProduct = "Cheese";
         int shipmentAmount = 15;
+        String discontinuedProduct = "Cheese";
+        String lowQuaProd = "" ;
+        int lowQua = 0;
 
         HashMap<String, Integer> inventory = new HashMap<>();
         inventory.put("Milk", 4);
@@ -24,12 +31,49 @@ public class Main {
         }
 
         if(inventory.containsKey(searchProduct)) {
-            inventory.put(searchProduct, (inventory.get(searchProduct) + shipmentAmount));
+            inventory.put(searchProduct, inventory.get(searchProduct) + shipmentAmount);
             System.out.println(searchProduct + " restocked!");
             System.out.println(inventory.get(searchProduct));
         } else {
             System.out.println("Product not found.");
         }
+
+        if(inventory.containsKey(discontinuedProduct)) {
+            System.out.println(inventory.get(discontinuedProduct));
+            inventory.remove(discontinuedProduct);
+            if(!inventory.containsKey(discontinuedProduct)) {
+                System.out.println(discontinuedProduct + " discontinued.");
+            }
+        } else {
+            System.out.println("Product not found.");
+        }
+
+        for(Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue() + " units");
+        }
+
+        for(Map.Entry<String, Integer> entry : inventory.entrySet()) {
+                if(lowQuaProd == "") {
+                    lowQua = entry.getValue();
+                    lowQuaProd = entry.getKey();
+            }   else if(entry.getValue() < lowQua) {
+                    lowQua = entry.getValue();
+                    lowQuaProd = entry.getKey();
+            } else if(entry.getValue() == lowQua) {
+                lowQuaProd = entry.getKey() + " & " + lowQuaProd;
+            }
+        }
+
+        if(lowQuaProd == "") {
+            System.out.println("Product not found.");
+        } else {
+            System.out.println("LOWEST STOCK: " + lowQuaProd);
+            System.out.println("Quantity: " + lowQua);
+        }
+
+
+
+
     }
 
 }
